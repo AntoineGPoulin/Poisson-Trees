fab098f9102095b1ea41e4f7618c960dbcf76a84
AntoineGPoulin <antoine.g.poulin@gmail.com>
2023-04-04 07:34:57 -0400

    Forget that last commit, can't get modules to work

--- PoissonR.py
+++ PoissonR.py
@@ -19,6 +19,54 @@ seeds = None
 # Start the timer
 start_time = time.time()
 
+def poisson_point_process(rate, dimensions, region_sizes, seed=None):
+    """
+    Generate Poisson Point Process samples in arbitrary dimensions.
+    
+    Parameters:
+    rate (float): Intensity of the Poisson point process.
+    dimensions (int): Number of dimensions.
+    region_sizes (tuple): Size of the region in each dimension.
+    seed (int, optional): Seed for random number generation. Defaults to None.
+    
+    Returns:
+    numpy.ndarray: Array of points generated from the Poisson Point Process.
+    """
+    np.random.seed(seed)
+    volume = np.prod(region_sizes)
+    num_points = np.random.poisson(rate * volume)
+    points = np.random.rand(num_points, dimensions) * region_sizes
+    return points
+
+def compute_voronoi(points):
+    """
+    Compute Voronoi cells for the given points.
+    
+    Parameters:
+    points (numpy.ndarray): Array of points.
+    
+    Returns:
+    scipy.spatial.Voronoi: Voronoi tessellation.
+    """
+    vor = Voronoi(points)
+    return vor
+
+def generate_multilayer_poisson(rate, dimensions, region_sizes, height, seeds=None):
+    if seeds is None:
+        seeds = [None] * (height + 1)
+
+    point_layers = [poisson_point_process(rate ** i, dimensions, region_sizes, seed=seeds[i]) for i in range(1, height + 1)]
+    return point_layers
+
+def construct_graph(point_layers):
+    G = nx.Graph()
+    for i, points in enumerate(point_layers[:-1]):
+        tree = cKDTree(point_layers[i+1])
+        for point in points:
+            dist, idx = tree.query(point)
+            G.add_edge(tuple(point), tuple(point_layers[i+1][idx]))
+    return G
+
 def get_color_map(point_layers, G):
     color_map = {}
     
